560e7815a4a1b1289edc8519037d62fb3018844b
Ilya Ashchepkov <koctep@gmail.com>
2015-01-23 16:47:09 +0600

    added missed make_config.erl

---
+++ make_config.erl
@@ -0,0 +1,19 @@
+#!/usr/bin/escript
+
+main(_) ->
+  io:format("~p~n", [os:getenv("APPS")]),
+  Files = case os:getenv("APPS") of
+            false -> [];
+            F -> re:split(F, " ")
+          end,
+  Opts = main(Files, []),
+  io:format("~p.~n", [Opts]).
+
+main([], Opts) ->
+  lists:reverse(Opts);
+main([File | Rest], Opts) ->
+  {ok, [{application, App, PropList}]} = file:consult(File),
+  case proplists:get_value(env, PropList, []) of
+    [] -> main(Rest, Opts);
+    Env -> main(Rest, [{App, Env} | Opts])
+  end.
